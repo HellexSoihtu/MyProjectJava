5f58149ee95e66cbfc49f6d5d2ce3b5ec9fcc3d9
HellexSoihtu <87639923+HellexSoihtu@users.noreply.github.com>
2021-09-08 15:52:21 +0300

    Task1 commit

--- javaLessons/src/main/java/school/lesson1/MainClass.java
+++ javaLessons/src/main/java/school/lesson1/MainClass.java
@@ -1,4 +1,15 @@
 package school.lesson1;
 
 public class MainClass {
+    public static void main(String[] args) { // задание 1
+        printThreeWords(); // Создайте метод printThreeWords(),
+        // который при вызове должен отпечатать в столбец три слова:
+        // Orange, Banana, Apple.
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange"); // вывод слова "Orange"
+        System.out.println("Banana"); // вывод слова "Banana"
+        System.out.println("Apple");  // вывод слова "Apple"
+    }
 }
