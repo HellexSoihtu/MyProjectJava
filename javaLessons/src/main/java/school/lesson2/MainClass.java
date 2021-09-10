d21c4046ce51283f49cc74bbeb710bf7627c7363
HellexSoihtu <87639923+HellexSoihtu@users.noreply.github.com>
2021-09-10 15:17:12 +0300

    Lesson1, Lesson2 commit

--- javaLessons/src/main/java/school/lesson2/MainClass.java
+++ javaLessons/src/main/java/school/lesson2/MainClass.java
@@ -134,9 +134,26 @@ public class MainClass
             arr[i] = i + 1;
             System.out.println("arr[" + i + "] = " + arr[i]); // вывод элементов массива
         }
-
+        System.out.println();
     }
 
+    // задание 8
+    // Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+    // пройти по нему циклом, и числа меньше 6 умножить на 2;
+
+    public static void changeArray()
+    {
+        int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };  // задание массива
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (arr[i] < 6)
+            {
+                arr[i] *= 2;
+            }
+            System.out.print(" " + arr[i]);                   // вывод измененного массива
+        }
+        System.out.println();
+    }
     public static void main(String[] args)
     {
           within10and20(30, -5);
@@ -146,6 +163,7 @@ public class MainClass
           isLeap(2021);
           invertArray();
           fillArray();
+          changeArray();
     }
 }
 
